4a88ff7214cd86a95a6c9984459172019cdcd01d
dawncronin <github email address>
2019-12-10 09:30:57 -0800

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,25 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(time) {
+  let splitTime = time.split(":");
+  let hour = splitTime[0];
+  let min = splitTime[1];
+
+  if (hour < 12)
+    return "Good Morning";
+  else if (hour < 17)
+    return "Good Afternoon";
+  else 
+    return "Good Evening";
+};
+
+function displayMessage(input) {
+  let me = document.getElementById("greeting")
+  me.innerText = input
+  return me
+};
+
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
